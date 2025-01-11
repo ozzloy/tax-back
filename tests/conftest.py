c7157c536d98e2bb7adc537633bde6c11f17cd3f
daniel watson <ozzloy@each.do>
2025-01-11 06:45:30 -0800

    conform documentation to pylint standard

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,32 @@
+"""configure tests."""
+
 from flask.testing import FlaskClient
 import pytest
 
 from app import create_app
 from app.db import db
-from app.model import King, Theme
 from app.seed import seed
 from config import Config
 
 
 class TestConfig(Config):
+    """test configuration."""
+
     TESTING = True
     CHECK_EMAIL_DELIVERABILITY = False
 
 
 class CSRFTestClient(FlaskClient):
-    """implicitly add csrf token to all non-get requests"""
+    """implicitly add csrf token to all non-get requests."""
 
     def __init__(self, *args, **kwargs):
-        """create a csrf client"""
+        """Create a csrf client."""
         super().__init__(*args, **kwargs)
         self._csrf_token = None
 
     @property
     def csrf_token(self):
-        """return valid csrf token, requesting it if necessary"""
+        """Return valid csrf token, requesting it if necessary."""
         if not self._csrf_token:
             response = super().get("/api/csrf-token")
             if response.status_code != 200:
@@ -36,28 +39,32 @@ class CSRFTestClient(FlaskClient):
         return self._csrf_token
 
     def _prepare_request(self, kwargs):
-        """add csrf and set content type for all requests"""
+        """Add csrf and set content type for all requests."""
         kwargs.setdefault("headers", {})
         kwargs["content_type"] = "application/json"
         kwargs["headers"].setdefault("X-CSRF-TOKEN", self.csrf_token)
         return kwargs
 
     def post(self, *args, **kwargs):
+        """Add csrf, set content type to application/json to posts."""
         return super().post(*args, **self._prepare_request(kwargs))
 
     def put(self, *args, **kwargs):
+        """Add csrf, set content type to application/json to puts."""
         return super().put(*args, **self._prepare_request(kwargs))
 
     def patch(self, *args, **kwargs):
+        """Add csrf, set content type to application/json to patches."""
         return super().patch(*args, **self._prepare_request(kwargs))
 
     def delete(self, *args, **kwargs):
+        """Add csrf, set content type to application/json to deletes."""
         return super().delete(*args, **self._prepare_request(kwargs))
 
 
 @pytest.fixture
 def app():
-    """generate testing version of app"""
+    """Generate testing version of app."""
     app = create_app(TestConfig)
     app.test_client_class = CSRFTestClient
 
@@ -74,5 +81,5 @@ def app():
 
 @pytest.fixture
 def client(app):
-    """get test client"""
+    """Get test client."""
     return app.test_client()
